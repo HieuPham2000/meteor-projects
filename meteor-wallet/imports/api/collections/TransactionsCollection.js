e9e54fd9513ec3281c44999a5fa5c2372efc4061
HieuPham2000 <hieu.pt183535@gmail.com>
2024-01-02 17:01:04 +0700

    feat: [meteor-wallet] collection hooks with inheritance

--- meteor-wallet/imports/api/collections/TransactionsCollection.js
+++ meteor-wallet/imports/api/collections/TransactionsCollection.js
@@ -1,18 +1,65 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
-
-export const TransactionsCollection = new Mongo.Collection('transactions');
+import { WalletsCollection } from './WalletsCollection';
 
 export const TRANSFER_TYPE = 'TRANSFER';
 export const ADD_TYPE = 'ADD';
 
+class TransactionsMongoCollection extends Mongo.Collection {
+  insert(doc, callback) {
+    if (doc.type === TRANSFER_TYPE) {
+      const sourceWallet = WalletsCollection.findOne(doc.sourceWalletId);
+
+      // TODO: tạm thời chưa check destinationWallet, vì hiện tại chưa xử lý (đang fix tạo 1 wallet duy nhất khi run app)
+      // const destinationWallet = WalletsCollection.findOne(doc.destinationWalletId);
+
+      if (!sourceWallet) {
+        throw new Meteor.Error('Source wallet not found.');
+      }
+
+      // if (!destinationWallet) {
+      //   throw new Meteor.Error('Destination wallet not found.');
+      // }
+
+      if (sourceWallet.balance < doc.amount) {
+        throw new Meteor.Error('Insufficient funds.');
+      }
+
+      WalletsCollection.update(doc.sourceWalletId, {
+        $inc: { balance: -doc.amount },
+      });
+
+      WalletsCollection.update(doc.destinationWalletId, {
+        $inc: { balance: doc.amount },
+      });
+    }
+
+    if (doc.type === ADD_TYPE) {
+      const sourceWallet = WalletsCollection.findOne(doc.sourceWalletId);
+
+      if (!sourceWallet) {
+        throw new Meteor.Error('Source wallet not found.');
+      }
+
+      WalletsCollection.update(doc.sourceWalletId, {
+        $inc: { balance: doc.amount },
+      });
+    }
+
+    return super.insert(doc, callback);
+  }
+}
+
+export const TransactionsCollection = new TransactionsMongoCollection('transactions');
+
 const TransactionsSchema = new SimpleSchema({
   type: {
     type: String,
-    allowedValues: [TRANSFER_TYPE, ADD_TYPE]
+    allowedValues: [TRANSFER_TYPE, ADD_TYPE],
   },
   sourceWalletId: {
-    type: String
+    type: String,
     // regex Id
   },
   destinationWalletId: {
@@ -26,8 +73,8 @@ const TransactionsSchema = new SimpleSchema({
     exclusiveMin: true,
   },
   createdAt: {
-    type: Date
-  }
+    type: Date,
+  },
 });
 
 // @ts-ignore
