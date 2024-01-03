4dd62b183d3aca9a4b413bd9e84dd0f51c7c1041
HieuPham2000 <hieu.pt183535@gmail.com>
2024-01-03 11:20:21 +0700

    feat: [meteor-wallet] simple sign up

--- meteor-wallet/imports/ui/App.jsx
+++ meteor-wallet/imports/ui/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
+import { BrowserRouter } from 'react-router-dom';
 import { Header } from './Header';
-import { Wallet } from './Wallet';
+import { Router } from './Router';
 
 export const App = () => (
-  <div>
+  <BrowserRouter>
     <Header />
     <div className="min-h-full">
       <div className="max-w-4xl mx-auto p-2">
-        <Wallet />
-        <ContactForm />
-        <ContactList />
+        <Router />
       </div>
     </div>
-  </div>
+  </BrowserRouter>
 );
--- meteor-wallet/imports/ui/Header.jsx
+++ meteor-wallet/imports/ui/Header.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RoutePaths } from './RoutePaths';
+// @ts-ignore
+import { useLoggedUser } from 'meteor/quave:logged-user-react';
 
 export const Header = () => {
+  const navigate = useNavigate();
+  const { loggedUser, isLoadingLoggedUser } = useLoggedUser();
+
   return (
     <header className="bg-indigo-600">
       <nav className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
-          <div className="flex items-center">
-            <a href="#">
-              <span className="sr-only">Meteor Wallet</span>
-              <img
-                className="h-10 w-auto"
-                src="/images/logo.png"
-                alt=""
-              />
-            </a>
+          <div className="flex justify-between flex-grow items-center">
+            <div>
+              <a className="cursor-pointer" onClick={() => navigate(RoutePaths.HOME)}>
+                <span className="sr-only">Meteor Wallet</span>
+                <img className="h-10 w-auto" src="/images/logo.png" alt="" />
+              </a>
+            </div>
+
+            <div>
+              {!isLoadingLoggedUser && !loggedUser && (
+                <button type="button" className="text-white font-bold" onClick={() => navigate(RoutePaths.SIGN_UP)}>
+                  Sign Up
+                </button>
+              )}
+              {!isLoadingLoggedUser && loggedUser && (
+                <button type="button" className="text-white font-bold" onClick={() => Meteor.logout()}>
+                  Log Out
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
